203e0b1d9a74102e192465e3cc618b20eb5b4faf
gondodo <snowsmile.1217@gmail.com>
2016-06-23 23:27:42 +0900

    pre-pvPage commit

--- app/controllers/impression_controller.rb
+++ app/controllers/impression_controller.rb
@@ -2,6 +2,20 @@ class ImpressionController < ApplicationController
   protect_from_forgery with: :null_session
 
   def index
+    # 総PV数
+    @allcount_data = Impression.count
+    # URL別のView数
+    @each_url_data = Impression.group(:url).count
+    # browser別View数
+    @each_browser_data = Impression.group(:browser).count
+
+    # 時間帯別
+    # 総PVに対する時間帯別
+    @time_allcount_data = Impression.group_by_hour_of_day(:created_at, format: '%l %P').count
+    # URL別のView数に対する時間帯別
+    @time_each_url_data = Impression.group(:url).group_by_hour_of_day(:created_at, format: '%l %P').count
+    # browser別View数
+    @time_each_browser_data = Impression.group(:browser).group_by_hour_of_day(:created_at, format: '%l %P').count
   end
 
   def create
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+url = ['http://192.168.33.11:3000/422.html','http://192.168.33.11:3000/500.html','http://192.168.33.11:3000/','http://192.168.33.11:3000/404.html' ]
+browser = ['Internet Explorer','Safari', 'Opera']
+browser_version = ['10.0', '9.1.1', '11.50']
+platform = ['Windows', 'MacIntel', 'Win32']
+
+100.times do
+  i = rand(0..2)
+  u = url[i]
+  br = browser[i]
+  brv = browser_version[i]
+  pl = platform[i]
+  # created_at = "2016-0#{rand(1..5)}-#{rand(1..30)} #{rand(0..24)}:#{rand(0..59)}:#{rand(0..59)}"
+  created_at = "2016-06-#{rand(19..26)} #{rand(0..24)}:#{rand(0..59)}:#{rand(0..59)}"
+  Impression.create(url: u, browser: br, browser_version: brv, platform: pl, created_at: created_at)
+end
